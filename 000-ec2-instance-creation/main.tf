4affa43e9ff070c8fd766d14ed7978d909522c7e
Arafin Ahmed <arafin2021@gmail.com>
2025-05-02 22:23:49 +0600

    folder name changed

---
+++ 000-ec2-instance-creation/main.tf
@@ -0,0 +1,8 @@
+   provider "aws" {
+     region = "us-east-1"  # Set your desired AWS region
+   }
+
+   resource "aws_instance" "example" {
+     ami           = "ami-0f88e80871fd81e91"  # Specify an appropriate AMI ID
+     instance_type = "t2.micro"
+   }
